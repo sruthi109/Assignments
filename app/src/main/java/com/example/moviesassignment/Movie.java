327a23ab84e4b6948c84ea94fcf8d0d8aacb31c3
sruthi109 <sruthi.podapati@gmail.com>
2020-11-17 23:41:23 -0500

    Initial commit Done

---
+++ app/src/main/java/com/example/moviesassignment/Movie.java
@@ -0,0 +1,47 @@
+package com.example.moviesassignment;
+
+public class Movie {
+    private String mTitle;
+    private String mRatings;
+    private String mDescription;
+    private int mImage;
+
+    public Movie(String mTitle, String mRatings, String mDescription, int mImage) {
+        this.mTitle = mTitle;
+        this.mRatings = mRatings;
+        this.mDescription = mDescription;
+        this.mImage = mImage;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmRatings() {
+        return mRatings;
+    }
+
+    public void setmRatings(String mRatings) {
+        this.mRatings = mRatings;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public int getmImage() {
+        return mImage;
+    }
+
+    public void setmImage(int mImage) {
+        this.mImage = mImage;
+    }
+}
